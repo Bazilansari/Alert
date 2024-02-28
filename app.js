3c86930f2a8a908458529a1f40adb9a35a501f90
Bazilansari <154603848+Bazilansari@users.noreply.github.com>
2024-02-28 20:06:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+// Q1.
+
+// alert('<p>ujhsdgfhd</p>')
+
+
+// Q2.
+
+// document.write('<p>ujhsdgfhd</p>' )
+
+
+// Q3.
+
+// alert('ujhsdg \n fhd')
+
+
+
+// Q4.
+
+// alert('welcome to JS land..')
+// alert('Happy Coding!')
+
+
+// Q5.
+
+// console.log('i can run JS through my Web browsers console');
+
+
+// Q6.
+
+function showAlert() {
+    alert("This is an alert!")
+}
